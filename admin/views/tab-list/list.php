9cf41e8b8899b2bde40998f8def9d9712f5f05ac
julesbsz <j.bousrez@outlook.com>
2024-03-26 14:17:01 +0100

    feat: added backups list tab

---
+++ admin/views/tab-list/list.php
@@ -0,0 +1,5 @@
+<div id="tab-list-content">
+    <h2><?php _e('List of previous backups', 'wp-backup-plugin'); ?></h2>
+
+    <p><?php _e('You don\'t have any backup yet', 'wp-backup-plugin'); ?></p>
+</div>
